d5dc395a2c532e18297ed22010680a9b4d0771fd
roland255 <rolandemereje@gmail.com>
2026-04-28 22:36:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 app.post('/api/login', (req, res) => {
     const { identifier, password } = req.body;
     // This looks for a match in either the 'email' or 'nickname' field
